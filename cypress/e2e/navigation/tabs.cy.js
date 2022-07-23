e0065d5d0a472f22d68f6753dff7a422a47af815
Francis Dolan <francis.dolan@gmail.com>
2022-07-23 22:53:09 +0100

    Refactored rest of tests

--- cypress/e2e/navigation/tabs.cy.js
+++ cypress/e2e/navigation/tabs.cy.js
@@ -12,66 +12,26 @@ describe('can interact with tabs correctly', () => {
     })
 
     it('pressing left while on a tab focuses the tab to the left', () => {
-        cy.visit('/destination')
-
-        cy.get('[data-cy="tab-list"] > button').then($tab_list => {
-            cy.wrap($tab_list)
-              .eq(1)
-              .click()
-              .trigger('keydown', { keyCode: 37 })
-              .prev()
-              .should('have.focus')
-        })
+        cy.tabsCanGoLeft('destination')
+        // cy.tabsCanGoLeft('crew')
+        cy.tabsCanGoLeft('technology')
     })
 
     it('pressing right while on a tab focuses the tab to the right', () => {
-        cy.visit('/destination')
-
-        cy.get('[data-cy="tab-list"] > button').then($tab_list => {
-            cy.wrap($tab_list)
-              .eq(1)
-              .click()
-              .trigger('keydown', { keyCode: 39 })
-              .next()
-              .should('have.focus')
-        })
+        cy.tabsCanGoRight('destination')
+        // cy.tabsCanGoRight('crew')
+        cy.tabsCanGoRight('technology')
     })
 
     it('pressing left on the first tab focuses on the last tab', () => {
-        cy.visit('/destination')
-
-        cy.get('[data-cy="tab-list"] > button').as('tabs')
-
-        cy.get('@tabs').then($tab_list => {
-            cy.wrap($tab_list)
-              .first()
-              .click()
-              .trigger('keydown', { keyCode: 37 })
-        })
-
-        cy.get('@tabs').then($tab_list => {
-            cy.wrap($tab_list)
-              .last()
-              .should('have.focus')
-        })
+        cy.tabsCanLoopToTheEnd('destination')
+        // cy.tabsCanLoopToTheEnd('crew')
+        // cy.tabsCanLoopToTheEnd('technology')
     })
 
     it('pressing right on the last tab focuses on the first tab', () => {
-        cy.visit('/destination')
-
-        cy.get('[data-cy="tab-list"] > button').as('tabs')
-
-        cy.get('@tabs').then($tab_list => {
-            cy.wrap($tab_list)
-              .last()
-              .click()
-              .trigger('keydown', { keyCode: 39 })
-        })
-
-        cy.get('@tabs').then($tab_list => {
-            cy.wrap($tab_list)
-              .first()
-              .should('have.focus')
-        })
+        cy.tabsCanLoopToTheStart('destination')
+        // cy.tabsCanLoopToTheStart('crew')
+        cy.tabsCanLoopToTheStart('technology')
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,10 +31,10 @@ Cypress.Commands.add('tabsA11yIntial', (url) => {
             .and('have.attr', 'hidden')
           })
     })
-  })
+})
 
 
-  Cypress.Commands.add('canDisplayPanel', (url) => {
+Cypress.Commands.add('canDisplayPanel', (url) => {
     cy.visit('/' + url)
 
     cy.get('[data-cy="tab-list"] > button').then($tab_list => {
@@ -62,8 +62,73 @@ Cypress.Commands.add('tabsA11yIntial', (url) => {
            .siblings('[data-cy="tab-panel"]')
            .each(panel => {
             cy.wrap(panel)
-              .should('not.is.visible')
-              .and('have.attr', 'hidden')
+                .should('not.is.visible')
+                .and('have.attr', 'hidden')
            })
     })
-  })
+})
+
+
+Cypress.Commands.add('tabsCanGoLeft', (url) => {
+    cy.visit('/' + url)
+
+    cy.get('[data-cy="tab-list"] > button').then($tabs => {
+        cy.wrap($tabs)
+          .eq(1)
+          .click()
+          .trigger('keydown', { keyCode: 37 })
+          .prev()
+          .should('have.focus')
+    })
+})
+
+Cypress.Commands.add('tabsCanGoRight', (url) => {
+    cy.visit('/' + url)
+
+    cy.get('[data-cy="tab-list"] > button').then($tab_list => {
+        cy.wrap($tab_list)
+          .eq(1)
+          .click()
+          .trigger('keydown', { keyCode: 39 })
+          .next()
+          .should('have.focus')
+    })
+})
+
+Cypress.Commands.add('tabsCanLoopToTheEnd', (url) => {
+    cy.visit('/' + url)
+
+    cy.get('[data-cy="tab-list"] > button').as('tabs')
+
+    cy.get('@tabs').then($tab_list => {
+        cy.wrap($tab_list)
+          .first()
+          .click()
+          .trigger('keydown', { keyCode: 37 })
+    })
+
+    cy.get('@tabs').then($tab_list => {
+        cy.wrap($tab_list)
+          .last()
+          .should('have.focus')
+    })
+})
+
+Cypress.Commands.add('tabsCanLoopToTheStart', (url) => {
+    cy.visit('/' + url)
+
+    cy.get('[data-cy="tab-list"] > button').as('tabs')
+
+    cy.get('@tabs').then($tab_list => {
+        cy.wrap($tab_list)
+          .last()
+          .click()
+          .trigger('keydown', { keyCode: 39 })
+    })
+
+    cy.get('@tabs').then($tab_list => {
+        cy.wrap($tab_list)
+          .first()
+          .should('have.focus')
+    })
+})
