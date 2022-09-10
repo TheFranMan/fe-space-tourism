a9ff0014f7d4898a281d5fed5fe84d45717d8956
Francis Dolan <francis.dolan@gmail.com>
2022-09-10 16:50:00 +0100

    Added crew tabs tests

--- cypress/e2e/tabs/tabs.cy.js
+++ cypress/e2e/tabs/tabs.cy.js
@@ -7,31 +7,31 @@ describe('can interact with tabs correctly', () => {
 
     it('clicking on a tab displays the new tab panel', () => {
         cy.canDisplayPanel('destination')
-        // cy.canDisplayPanel('crew')
+        cy.canDisplayPanel('crew')
         // cy.canDisplayPanel('technology')
     })
 
     it('pressing left while on a tab focuses the tab to the left', () => {
         cy.tabsCanGoLeft('destination')
-        // cy.tabsCanGoLeft('crew')
+        cy.tabsCanGoLeft('crew')
         cy.tabsCanGoLeft('technology')
     })
 
     it('pressing right while on a tab focuses the tab to the right', () => {
         cy.tabsCanGoRight('destination')
-        // cy.tabsCanGoRight('crew')
+        cy.tabsCanGoRight('crew')
         cy.tabsCanGoRight('technology')
     })
 
     it('pressing left on the first tab focuses on the last tab', () => {
         cy.tabsCanLoopToTheEnd('destination')
-        // cy.tabsCanLoopToTheEnd('crew')
+        cy.tabsCanLoopToTheEnd('crew')
         // cy.tabsCanLoopToTheEnd('technology')
     })
 
     it('pressing right on the last tab focuses on the first tab', () => {
         cy.tabsCanLoopToTheStart('destination')
-        // cy.tabsCanLoopToTheStart('crew')
+        cy.tabsCanLoopToTheStart('crew')
         cy.tabsCanLoopToTheStart('technology')
     })
 })
