e8c891f0765acc8f6c031767720561ecc1a8bd25
Francis Dolan <francis.dolan@gmail.com>
2022-09-17 17:16:25 +0100

    Enabled tabs test in technology page

--- cypress/e2e/tabs/tabs.cy.js
+++ cypress/e2e/tabs/tabs.cy.js
@@ -2,13 +2,13 @@ describe('can interact with tabs correctly', () => {
     it('tabs initally render with the correct a11y attributes set', () => {
         cy.tabsA11yIntial('destination')
         cy.tabsA11yIntial('crew')
-        // cy.tabsA11yIntial('technology')
+        cy.tabsA11yIntial('technology')
     })
 
     it('clicking on a tab displays the new tab panel', () => {
         cy.canDisplayPanel('destination')
         cy.canDisplayPanel('crew')
-        // cy.canDisplayPanel('technology')
+        cy.canDisplayPanel('technology')
     })
 
     it('pressing left while on a tab focuses the tab to the left', () => {
@@ -26,7 +26,7 @@ describe('can interact with tabs correctly', () => {
     it('pressing left on the first tab focuses on the last tab', () => {
         cy.tabsCanLoopToTheEnd('destination')
         cy.tabsCanLoopToTheEnd('crew')
-        // cy.tabsCanLoopToTheEnd('technology')
+        cy.tabsCanLoopToTheEnd('technology')
     })
 
     it('pressing right on the last tab focuses on the first tab', () => {
